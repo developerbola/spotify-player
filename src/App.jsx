2683faed767bc570b3d0baa3bee3748e8a077982
developerbola <developerbola08@gmail.com>
2025-04-27 17:23:29 +0500

    #12 Fix app window location memorize bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import { useEffect } from "react";
-import { appWindow } from "@tauri-apps/api/window";
+import { useEffect, useRef, useState } from "react";
+import { appWindow, PhysicalPosition } from "@tauri-apps/api/window";
 import SpotifyPlayer from "./SpotifyPlayer";
 import "./styles/index.css";
 
 const App = () => {
+  const lastPosition = useRef({ x: null, y: null });
+
   useEffect(() => {
-    const x = parseInt(localStorage.getItem("playerPositionX"));
-    const y = parseInt(localStorage.getItem("playerPositionY"));
+    const storedX = localStorage.getItem("playerPositionX");
+    const storedY = localStorage.getItem("playerPositionY");
+    console.log({ storedX, storedY });
 
-    appWindow.setPosition(x, y);
-
-    let lastPosition = { x: null, y: null };
+    if (storedX !== null && storedY !== null) {
+      const x = parseInt(storedX, 10);
+      const y = parseInt(storedY, 10);
+      if (!isNaN(x) && !isNaN(y)) {
+        // Ensure you pass a PhysicalPosition or LogicalPosition
+        appWindow.setPosition(new PhysicalPosition(x, y));
+      }
+    }
 
     const checkWindowPosition = async () => {
       const position = await appWindow.innerPosition();
-      if (position.x !== lastPosition.x || position.y !== lastPosition.y) {
+
+      if (
+        position.x !== lastPosition.current.x ||
+        position.y !== lastPosition.current.y
+      ) {
+        // Save the new position to localStorage
         localStorage.setItem("playerPositionX", position.x);
         localStorage.setItem("playerPositionY", position.y);
-        lastPosition = position;
+
+        // Update the lastPosition reference
+        lastPosition.current = position;
       }
     };
 
-    const interval = setInterval(checkWindowPosition, 100);
+    const interval = setInterval(checkWindowPosition, 1000);
 
     return () => {
       clearInterval(interval);

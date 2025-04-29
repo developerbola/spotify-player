dd5d5bf4460777b1c7770c89164b0a48a00db5a5
developerbola <developerbola08@gmail.com>
2025-04-29 18:32:35 +0500

    #17 Fix controller size

--- src/SpotifyPlayer.jsx
+++ src/SpotifyPlayer.jsx
@@ -265,8 +265,8 @@ const SpotifyPlayer = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 384 512"
                 className="spotify-control"
-                height={16}
-                width={18}
+                height={17}
+                width={17}
                 onClick={() => commandSpotify("play")}
               >
                 <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
@@ -276,8 +276,8 @@ const SpotifyPlayer = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
                 className="spotify-control"
-                height={18}
-                width={18}
+                height={17}
+                width={17}
                 style={{ marginBottom: 1 }}
                 onClick={() => commandSpotify("pause")}
               >
